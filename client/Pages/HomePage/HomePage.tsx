2b419524b6fbb9f9d8015a717a12b4de9a05ccbc
harpreet slatch <blueslatch90@gmail.com>
2025-01-16 16:57:41 +1300

    upate homepage for responsive

--- client/Pages/HomePage/HomePage.tsx
+++ client/Pages/HomePage/HomePage.tsx
@@ -9,7 +9,7 @@ export function HomePage() {
           <img src="/image/slatch-2.png" alt="my-pic" />
         </div>
         <div>
-          <h1 className="mt-28 flex justify-center font-nohemiLight text-[7rem] lg:text-[12rem] 2xl:mt-[13rem] 2xl:text-[24.25rem]">
+          <h1 className="mt-28 flex w-full justify-center text-center font-nohemiLight sm:text-[13vw] md:text-[13vw] lg:text-[13vw] xl:text-[13vw] 2xl:text-[13vw]">
             Harpreet Slatch
           </h1>
         </div>
