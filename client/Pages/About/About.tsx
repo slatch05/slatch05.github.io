0dbeaeba5b0cf25ef0664373002eb0020c8a724d
harpreet slatch <blueslatch90@gmail.com>
2025-01-18 12:36:22 +1300

    responsive about me

--- client/Pages/About/About.tsx
+++ client/Pages/About/About.tsx
@@ -2,7 +2,7 @@ export function About() {
   return (
     <div className="mt-20">
       <div>
-        <div className="text-5xl font-light sm:text-5xl xl:text-5xl 2xl:text-[5rem]">
+        <div className="text-5xl font-light sm:text-7xl xl:text-5xl 2xl:text-[5rem]">
           I’m a full-stack web developer. I specialize in creating dynamic,{' '}
           user-friendly web applications using
           <span className="mx-2 text-[rgb(255,47,1)] ">
@@ -29,7 +29,7 @@ export function About() {
             />
           </div>
           <div className="mt-8 sm:mt-0">
-            <div className="w-full text-sm font-light uppercase sm:w-[45rem] lg:w-[50rem] 2xl:text-lg">
+            <div className="w-full font-light uppercase sm:w-[45rem] sm:text-xl lg:w-[50rem] lg:text-sm 2xl:text-xl">
               My journey into development started with years of leadership
               experience, where I managed teams, optimized processes, and solved
               challenges creatively. This background has shaped my approach to
