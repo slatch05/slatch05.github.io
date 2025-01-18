a8c1505bb43ce816917759184b657d7e8be69ced
harpreet slatch <blueslatch90@gmail.com>
2025-01-19 12:05:48 +1300

    changed homepage for movile view

--- client/Pages/HomePage/HomePage.tsx
+++ client/Pages/HomePage/HomePage.tsx
@@ -3,38 +3,37 @@ import { DrawerMenu } from '../../components/DrawerMenu'
 export function HomePage() {
   return (
     <div className="mt-32">
-      <div className="flex flex-row-reverse lg:flex-col lg:items-center lg:text-center">
+      <div className="flex flex-col-reverse lg:flex-col lg:items-center lg:text-center">
         {/* Image Section */}
-        <div className="w-50 lg:w-50 2xl:w-70 h-auto sm:w-80 md:w-60 xl:w-60">
+        <div className="w-50 lg:w-50 2xl:w-70 md:w-70 h-auto sm:mt-[20rem] sm:w-80 lg:mt-[0rem] xl:w-60">
+          <div className="flex w-full items-end sm:block lg:hidden">
+            <div className=" fixed right-7 mt-[27rem] w-fit rounded-full border border-solid border-black px-6 py-1 text-center text-lg uppercase transition-all duration-300 ease-in-out hover:bg-black hover:text-white sm:text-5xl lg:text-center ">
+              <DrawerMenu />
+            </div>
+          </div>
           <img
             className="h-auto w-full object-cover"
             src="/image/slatch-2.png"
             alt="my-pic"
           />
         </div>
+
+        <div className="mt-[4rem] w-[24rem] text-pretty sm:block sm:text-3xl lg:hidden lg:text-center lg:text-xl 2xl:text-2xl">
+          I’m always ready for new projects and collaborations!
+          <div className="mt-8">
+            <a
+              className="w-fit rounded-full border border-solid border-black px-2 py-1 text-center text-lg uppercase transition-all duration-300 ease-in-out hover:bg-black hover:text-white sm:text-5xl lg:text-center "
+              href="mailto: blue_slatch@icloud.com"
+            >
+              BLUE_SLATCH@ICLOUD.COM
+            </a>
+          </div>
+        </div>
         {/* Text Section */}
         <div className=" flex items-center justify-start lg:mt-32">
-          <h1 className="text-left font-nohemiLight sm:text-[20vw] sm:leading-[1.1] md:text-[13vw] lg:text-[13vw] xl:text-[13vw] 2xl:text-[13vw]">
+          <h1 className="text-left font-nohemiLight sm:text-[20vw] sm:leading-[1.1] md:text-[16vw] lg:text-[13vw] xl:text-[13vw] 2xl:text-[13vw]">
             Harpreet Slatch
           </h1>
-        </div>
-      </div>
-
-      <div className="mt-[4rem] w-[24rem] text-pretty sm:block sm:text-3xl lg:hidden lg:text-center lg:text-xl 2xl:text-2xl">
-        I’m always ready for new projects and collaborations!
-        <div className="mt-8">
-          <a
-            className="w-fit rounded-full border border-solid border-black px-2 py-1 text-center text-lg uppercase transition-all duration-300 ease-in-out hover:bg-black hover:text-white sm:text-5xl lg:text-center "
-            href="mailto: blue_slatch@icloud.com"
-          >
-            BLUE_SLATCH@ICLOUD.COM
-          </a>
-        </div>
-      </div>
-
-      <div className="mt-[50rem] flex w-full items-end sm:block lg:hidden">
-        <div className="absolute right-7 w-fit rounded-full border border-solid border-black px-6 py-1 text-center text-lg uppercase transition-all duration-300 ease-in-out hover:bg-black hover:text-white sm:text-5xl lg:text-center ">
-          <DrawerMenu />
         </div>
       </div>
     </div>
