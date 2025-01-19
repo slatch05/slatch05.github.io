c68e475b49c6c25f168d876ff640ee6e249f9eac
harpreet slatch <blueslatch90@gmail.com>
2025-01-19 17:34:56 +1300

    delete text from home page

--- client/Pages/HomePage/HomePage.tsx
+++ client/Pages/HomePage/HomePage.tsx
@@ -5,7 +5,7 @@ export function HomePage() {
     <div className="mt-32">
       <div className="flex flex-col-reverse lg:flex-col lg:items-center lg:text-center">
         {/* Image Section */}
-        <div className="w-50 lg:w-50 md:w-50 h-auto sm:mt-[20rem] sm:w-80 lg:mt-[0rem] xl:w-60 2xl:w-80 ">
+        <div className="w-50 lg:w-50 md:w-50 h-auto sm:mt-[30rem] sm:w-80 lg:mt-[0rem] xl:w-60 2xl:w-80 ">
           <div className="flex w-full items-end sm:block lg:hidden">
             <div className=" fixed right-7 mt-[27rem] w-fit rounded-full border border-solid border-black px-4 py-1 text-center text-lg uppercase transition-all duration-300 ease-in-out hover:bg-black hover:text-white sm:text-5xl lg:text-center ">
               <DrawerMenu />
@@ -18,17 +18,15 @@ export function HomePage() {
           />
         </div>
 
-        <div className="mt-[4rem] w-[24rem] text-pretty sm:block sm:text-3xl lg:hidden lg:text-center lg:text-xl 2xl:text-2xl">
-          I’m always ready for new projects and collaborations!
-          <div className="mt-8">
-            <a
-              className="w-fit rounded-full border border-solid border-black px-4 py-1 text-center text-lg uppercase transition-all duration-300 ease-in-out hover:bg-black hover:text-white sm:text-5xl lg:text-center "
-              href="mailto: blue_slatch@icloud.com"
-            >
-              BLUE_SLATCH@ICLOUD.COM
-            </a>
-          </div>
+        <div className="mt-8">
+          <a
+            className="w-fit rounded-full border border-solid border-black px-4 py-1 text-center text-lg uppercase transition-all duration-300 ease-in-out hover:bg-black hover:text-white sm:text-5xl lg:text-center "
+            href="mailto: blue_slatch@icloud.com"
+          >
+            BLUE_SLATCH@ICLOUD.COM
+          </a>
         </div>
+
         {/* Text Section */}
         <div className=" flex items-center justify-start lg:mt-32">
           <h1 className="text-left font-nohemiLight sm:text-[20vw] sm:leading-[1.1] md:text-[16vw] lg:text-[13vw] xl:text-[13vw] 2xl:text-[13vw]">
