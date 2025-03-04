6dbf50ffca05db705644e2aaaf7b7caa6b54d103
harpreet slatch <blueslatch90@gmail.com>
2025-03-05 09:57:31 +1300

    update resume

--- client/Pages/Footer/Footer.tsx
+++ client/Pages/Footer/Footer.tsx
@@ -55,7 +55,7 @@ export function Footer() {
               </span>
               <div className="uppercase">
                 <a
-                  href="https://sg.docworkspace.com/d/cINaDnq-fApmHlb4G?utm_source=wps_office_mac&utm_medium=comp_border_bar&utm_content=link"
+                  href="https://sg.docworkspace.com/d/cINaDnq-fApmHlb4G?utm_source=wps_office_mac&utm_medium=&utm_content=link"
                   className="group relative mr-1"
                 >
                   Resume,
